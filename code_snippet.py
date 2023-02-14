f2c1f5f11b07b4bf9704743e14b4ad1f69363f22
dbtmd324 <dbtmd324@gmail.com>
2023-02-14 23:51:58 +0900

    code snippet changed

--- code_snippet.py
+++ code_snippet.py
@@ -267,3 +267,21 @@ def mainMC(numObs=10, numBars=100, maxH=5, numIter=1E6, numThreads=24):
     print (pd.DataFrame(out).describe())
 
     return
+
+#### SNIPPET 4.10: DETERINATION OF SAMPLE WEIGHT BY ABSOLUTE RETURN ATTRIBUTION ####
+
+def mpSampleW(t1, numCoEvents, close, molecule):
+    # Derive sample weight by return attribution
+    ret = np.log(close).diff() # log-returns, so that they are additive
+    wght = pd.Series(index=molecule)
+
+    for tIn, tOut in t1.loc[wght.index].iteritems():
+        wght.loc[tIn] = (ret.loc[tIn: tOut] / numCoEvents.loc[tIn: tOut]).sum()
+    
+    return wght.abs()
+
+out['w'] = mpPandasObj(mpSampleW, ('molecule', events.index), numThreads, t1 = events['t1'], numCoEvents = numCoEvents, close=close)
+out['w'] = out.shape[0] / out['w'].sum()
+
+        
+
