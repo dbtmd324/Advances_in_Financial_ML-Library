9a1f8d985f1431144b8c1bbc5aa29436997b098b
dbtmd324 <dbtmd324@gmail.com>
2023-02-16 16:56:58 +0900

    code snippet changed: Add snippet 5

--- code_snippet.py
+++ code_snippet.py
@@ -304,4 +304,28 @@ def getTimeDecay(tW, clfLastW=1):
 
 #### SNIPPET 5.1: WEIGHTING FUNCTION ####
 def getWeights(d, size):
-     
+    # thres > 0 drops insignificant weights
+    w = [1.]
+    for k in range(1, size):
+        w_ = w[-1] / k * (d-k+1)
+        w.append(w_)
+
+    w = np.array(w[::-1]).reshape(-1, 1)
+
+    return w
+
+def plotWeights(dRange, nPlots, size):
+    w = pd.DataFrame()
+    for d in np.linspace(dRange[0], dRange[1], nPlots):
+        w_ = getWeights(d, size=size)
+        w_ = pd.DataFrame(w_, index=range(w_.shape[0])[::-1], columns=[d])
+        w = w.join(w_, how='outer')
+
+    ax = w.plot()
+    ax.legend(loc='upper left');mpl.show()
+
+    return
+
+if __name__ == '__main__':
+    plotWeights(dRange=[0,1], nPlots=11, size=6)
+    plotWeights(dRange=[1,2], nPlots=11, size=6)
