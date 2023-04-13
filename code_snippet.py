146d3e2f97b477b4844f29b1ef66fffde1b99e2e
dbtmd324 <dbtmd324@gmail.com>
2023-04-13 17:42:05 +0900

    Add more codes in code_snippet.

--- code_snippet.py
+++ code_snippet.py
@@ -510,3 +510,56 @@ def cvScore(clf, X, y, sample_weight, scoring='neg_log_loss', t1=None, cv=None, 
         score.append(score_)
 
     return np.array(score)
+
+#### SNIPPET 8.2 MDI FEATURE IMPORTANCE ####
+
+def featImpMDI(fit, featNames):
+    # feat importance based on IS mean impurity reduction
+    df0 = {i: tree.feature_importances_ for i, tree in enumerate(fit.estimators_)}
+    df0 = pd.DataFrame.from_dict(df0, orient='index')
+    df0.columns = featNames
+    df0 = df0.replace(0, np.nan) # because max_features = 1
+    imp = pd.concat({'mean': df0.mean(), 'std': df0.std() * df0.shape[0]**(-0.5)}, axis=1)
+    imp /= imp['mean'].sum()
+    return imp
+
+#### SNIPPET 8.3 MDA FEATURE IMPORTANCE ####
+
+def featImpMDA(clf, X, y, cv, sample_weight, t1, pctEmbargo, scoring='neg_log_loss'):
+    # feat importance based on OOS score reduction
+    if scoring not in ['neg_log_loss', 'accuracy']:
+        raise Exception('wrong scoring method.')
+    
+    from sklearn.metrics import log_loss, accuracy_score
+    cvGen = PurgedKFold(n_splits=cv, t1=t1, pctEmbargo=pctEmbargo) # purged cv
+    scr0, scr1 = pd.Series(), pd.DataFrame(columns=X.columns)
+
+    for i, (train, test) in enumerate(cvGen.split(X=X)):
+        X0, y0, w0 = X.iloc[train,:], y.iloc[train], sample_weight.iloc[train]
+        X1, y1, w1 = X.iloc[test,:], y.iloc[test], sample_weight.iloc[test]
+        fit = clf.fit(X=X0, y=y0, sample_weight=w0.values)
+        if scoring == 'neg_log_loss':
+            prob = fit.predict_proba(X1)
+            scr0.loc[i] = -log_loss(y1, prob, sample_weight=w1.values, labels=clf.classes_)
+        else:
+            pred = fit.predict(X1)
+            scr0.loc[i] = accuracy_score(y1, pred, sample_weight=w1.values)
+
+        for j in X.columns:
+            X1_ = X1.copy(deep=True)
+            np.random.shuffle(X1_[j].values) # permutation of a single column
+            if scoring == 'neg_log_loss':
+                prob = fit.predict_proba(X1_)
+                scr1.loc[i,j] = -log_loss(y1, prob, sample_weight=w1.values, labels=clf.classes_)
+            else:
+                pred = fit.predict(X1_)
+                scr1.loc[i,j] = accuracy_score(y1, pred, sample_weight=w1.values)
+    imp = (-scr1).add(scr0, axis=0)
+
+    if scoring == 'neg_log_loss':
+        imp = imp/-scr1
+    else:
+        imp = imp/(1.-scr1)
+    imp = pd.concat({'mean': imp.mean(), 'std': imp.std() * imp.shape[0]**(-0.5)}, axis=1)
+    
+    return imp, scr0.mean()
